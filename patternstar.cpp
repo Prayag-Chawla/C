a0bf79d169527af1c132930dc662551a98e1e308
Prayag-Chawla <92213377+Prayag-Chawla@users.noreply.github.com>
2022-01-23 03:31:22 +0530

    Add files via upload

---
+++ patternstar.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int n;
+    cin >> n;
+    int currRow = 1;
+    while (currRow <= n - currRow)
+    {
+        int spaces = 1;
+    
+    
+    while(spaces <= n - currRow){
+        cout << " ",
+        spaces++;
+
+
+ 
+    }
+    int currCol = 1;
+    while (currCol <= (2 * currRow) - 1)
+    {
+        cout << '*';
+        currCol++;
+
+
+    }
+    cout << endl;
+    currRow++;
+}
+    
+}
