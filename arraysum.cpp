e60e40e74b80a4fabc4f47d1520f2c99eb9365ee
Prayag-Chawla <92213377+Prayag-Chawla@users.noreply.github.com>
2022-03-13 00:26:39 +0530

    Add files via upload

---
+++ arraysum.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int size;
+    cin >> size;
+    int arr[1000000];
+    for(int i = 0; i < size; i++){
+        cin >> arr[i];
+    }
+    int result = 0;
+    for(int i = 0; i < size; i++){
+        result += arr[i];
+    }
+    cout << result << endl;
+}
---
+++ leadersinarray.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+void leaders(int*arr,int len)
+{
+    int j = 0;
+    int *save = new int[len];
+    int largest=INT_MIN;
+    for(int i=len-1;i>=0;i--)
+{
+    if(arr[i]>=largest)
+    {
+        save[j]=arr[i];
+        j++;
+        largest=arr[i];
+    }
+}
+for(int i=j-1;i>=0;i--)
+{
+    for(int i=j-1;i>=0;i--)
+    {
+        cout<<save[i] << " ";
+    }
+}
+
+    
+
