2620ab42e1c897e6bb57293e1cb4f44704b983fb
Prayag-Chawla <92213377+Prayag-Chawla@users.noreply.github.com>
2022-04-13 12:02:47 +0530

    Add files via upload

--- 3d.cpp
+++ 3d.cpp
@@ -1,35 +1,14 @@
-#include<iostream>
+#include <new>
+#include <iostream>
 using namespace std;
-  
-class A
-{
-protected:
-    int x;
-public:
-    A() { x = 10; }
+class A {
+  public:
+    A() { cout << "A::A()" << endl; }
+    ~A() { cout << "A::~A()" << endl; }
 };
-  
-class B
-{
-protected:
-    int x;
-public:
-    B() { x = 20; }
-};
-  
-class C: public A, public B
-{
-public:
-   void fun()
-   {
-      cout << "A's x is " << A::x;
-      cout << "\nB's x is " << B::x;
-   }
-};
-  
-int main()
-{
-    C c;
-    c.fun();
-    return 0;
+int main () {
+  char* p = new char[sizeof(A)];
+  A* ap = new (p) A;
+  ap->A::~A();
+  delete [] p;
 }
