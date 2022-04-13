cabeedc908320a981e4f753302f1f9db09c313c3
Prayag-Chawla <92213377+Prayag-Chawla@users.noreply.github.com>
2022-04-13 11:36:42 +0530

    Add files via upload

--- 3a.cpp
+++ 3a.cpp
@@ -1,21 +1,31 @@
-#include<iostream>
-using namespace std;
-  
-class A 
-{
-public: 
-  
-   void fun();
+#include <iostream>
+#include <string>
+#include <vector>
+
+using std::cout; using std::endl;
+using std::vector; using std::string;
+
+class CustomString {
+public:
+    explicit CustomString(const string &s = string()):
+            str(new string(s)) { }
+
+    CustomString(const CustomString &p):
+            str(new string(*p.str)) { }
+
+    ~CustomString() { delete str; }
+
+    string & getString() { return *str; };
+private:
+    string *str;
 };
-  
-void A::fun()
-{
-   cout << "fun() called";
-}
-  
+
 int main()
 {
-   A a;
-   a.fun();
-   return 0;
+    CustomString str1("Not everyone in this world has got a halo");
+    CustomString str2(str1);
+
+    cout << "str1: " << str1.getString() << endl;
+    cout << "str2: " << str2.getString() << endl << endl;
+
 }
