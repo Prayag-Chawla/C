ddbe20663d8b55cbd00ecff1e1ccd6424e90bce2
Prayag-Chawla <92213377+Prayag-Chawla@users.noreply.github.com>
2022-04-27 12:32:24 +0530

    Add files via upload

--- 3b.cpp
+++ 3b.cpp
@@ -1,37 +1,13 @@
-#include<iostream>
-using namespace std;
- 
-class Circle
-{  
-    // private data member
-    private:
-        double radius;
-      
-    // public member function   
-    public:   
-        void compute_area(double r)
-        {   // member function can access private
-            // data member radius
-            radius = r;
-             
-            double area = 3.14*radius*radius;
-             
-            cout << "Radius is: " << radius << endl;
-            cout << "Area is: " << area;
-        }
-     
+class AB {
+public:
+  virtual void f() = 0;
 };
- 
-// main function
-int main()
-{  
-    // creating object of the class
-    Circle obj;
-     
-    // trying to access private data member
-    // directly outside the class
-    obj.compute_area(1.5);
-     
-     
-    return 0;
+
+class D2 : public AB {
+  void g();
+};
+
+int main() {
+  D2 d;
+}Base;
 }
